1423af2c423e903b1d790d81ff3c46bbf5160c35
Faisal-pixel <adamsfaisal2003@gmail.com>
2023-08-28 13:25:42 +0100

    fixed the notification bug

--- src/components/notifications-parent.component.jsx
+++ src/components/notifications-parent.component.jsx
@@ -5,12 +5,13 @@ const NotificationsParentComponent = ({onClose, isNotificationParentOpen}) => {
     const [slideIn, setSlideIn] = useState(false);
     useEffect(() => {
         setSlideIn(true)
-    }, [])
+        console.log(slideIn)
+    }, [slideIn])
     return <>
         <div className="fixed h-screen w-screen">
             <div className="bg-black bg-opacity-50 absolute inset-0" onClick={onClose}></div>
             <div className="fixed h-full max-w-[27%] right-0 pt-16 pr-8 space-y-2 overflow-y-auto overflow-x-hidden">
-                <div className={`bg-white pl-2 flex rounded translate-x-[150%] transition ${slideIn && "translate-x-0"}`}>
+                <div className={`bg-white pl-2 flex rounded transition ${slideIn ? "translate-x-0" : "translate-x-[150%]"}`}>
                     <div className="py-2">
                         <NotificationSvg className="pr-2"/>
                     </div>
@@ -24,202 +25,7 @@ const NotificationsParentComponent = ({onClose, isNotificationParentOpen}) => {
                         <div className="text-greyDark h-1/2 flex justify-center items-center">Hide</div>
                     </div>
                 </div>
-                <div className={`bg-white pl-2 flex rounded translate-x-[150%] transition ${slideIn && "translate-x-0"}`}>
-                    <div className="py-2">
-                        <NotificationSvg className="pr-2"/>
-                    </div>
-                    <div className="pr-7 py-2 space-y-1">
-                        <h3 className="font-bold">New RFQ!</h3>
-                        <p className="text-greyDark">Mark John just created a new RFQ request</p>
-                    </div>
-                    <div className="relative py-2 pr-2 pl-2 border-l-[3px] border-gray-200">
-                        <div className="text-primaryBlue w-max h-1/2 flex items-center">See Details</div>
-                        <hr className="h-[3px] w-full absolute top-[50%] left-0 bg-gray-200"/>
-                        <div className="text-greyDark h-1/2 flex justify-center items-center">Hide</div>
-                    </div>
-                </div>
-                <div className={`bg-white pl-2 flex rounded translate-x-[150%] transition ${slideIn && "translate-x-0"}`}>
-                    <div className="py-2">
-                        <NotificationSvg className="pr-2"/>
-                    </div>
-                    <div className="pr-7 py-2 space-y-1">
-                        <h3 className="font-bold">New RFQ!</h3>
-                        <p className="text-greyDark">Mark John just created a new RFQ request</p>
-                    </div>
-                    <div className="relative py-2 pr-2 pl-2 border-l-[3px] border-gray-200">
-                        <div className="text-primaryBlue w-max h-1/2 flex items-center">See Details</div>
-                        <hr className="h-[3px] w-full absolute top-[50%] left-0 bg-gray-200"/>
-                        <div className="text-greyDark h-1/2 flex justify-center items-center">Hide</div>
-                    </div>
-                </div>
-                <div className={`bg-white pl-2 flex rounded translate-x-[150%] transition ${slideIn && "translate-x-0"}`}>
-                    <div className="py-2">
-                        <NotificationSvg className="pr-2"/>
-                    </div>
-                    <div className="pr-7 py-2 space-y-1">
-                        <h3 className="font-bold">New RFQ!</h3>
-                        <p className="text-greyDark">Mark John just created a new RFQ request</p>
-                    </div>
-                    <div className="relative py-2 pr-2 pl-2 border-l-[3px] border-gray-200">
-                        <div className="text-primaryBlue w-max h-1/2 flex items-center">See Details</div>
-                        <hr className="h-[3px] w-full absolute top-[50%] left-0 bg-gray-200"/>
-                        <div className="text-greyDark h-1/2 flex justify-center items-center">Hide</div>
-                    </div>
-                </div>
-                <div className={`bg-white pl-2 flex rounded translate-x-[150%] transition ${slideIn && "translate-x-0"}`}>
-                    <div className="py-2">
-                        <NotificationSvg className="pr-2"/>
-                    </div>
-                    <div className="pr-7 py-2 space-y-1">
-                        <h3 className="font-bold">New RFQ!</h3>
-                        <p className="text-greyDark">Mark John just created a new RFQ request</p>
-                    </div>
-                    <div className="relative py-2 pr-2 pl-2 border-l-[3px] border-gray-200">
-                        <div className="text-primaryBlue w-max h-1/2 flex items-center">See Details</div>
-                        <hr className="h-[3px] w-full absolute top-[50%] left-0 bg-gray-200"/>
-                        <div className="text-greyDark h-1/2 flex justify-center items-center">Hide</div>
-                    </div>
-                </div>
-                <div className={`bg-white pl-2 flex rounded translate-x-[150%] transition ${slideIn && "translate-x-0"}`}>
-                    <div className="py-2">
-                        <NotificationSvg className="pr-2"/>
-                    </div>
-                    <div className="pr-7 py-2 space-y-1">
-                        <h3 className="font-bold">New RFQ!</h3>
-                        <p className="text-greyDark">Mark John just created a new RFQ request</p>
-                    </div>
-                    <div className="relative py-2 pr-2 pl-2 border-l-[3px] border-gray-200">
-                        <div className="text-primaryBlue w-max h-1/2 flex items-center">See Details</div>
-                        <hr className="h-[3px] w-full absolute top-[50%] left-0 bg-gray-200"/>
-                        <div className="text-greyDark h-1/2 flex justify-center items-center">Hide</div>
-                    </div>
-                </div>
-                <div className={`bg-white pl-2 flex rounded translate-x-[150%] transition ${slideIn && "translate-x-0"}`}>
-                    <div className="py-2">
-                        <NotificationSvg className="pr-2"/>
-                    </div>
-                    <div className="pr-7 py-2 space-y-1">
-                        <h3 className="font-bold">New RFQ!</h3>
-                        <p className="text-greyDark">Mark John just created a new RFQ request</p>
-                    </div>
-                    <div className="relative py-2 pr-2 pl-2 border-l-[3px] border-gray-200">
-                        <div className="text-primaryBlue w-max h-1/2 flex items-center">See Details</div>
-                        <hr className="h-[3px] w-full absolute top-[50%] left-0 bg-gray-200"/>
-                        <div className="text-greyDark h-1/2 flex justify-center items-center">Hide</div>
-                    </div>
-                </div>
-                <div className={`bg-white pl-2 flex rounded translate-x-[150%] transition ${slideIn && "translate-x-0"}`}>
-                    <div className="py-2">
-                        <NotificationSvg className="pr-2"/>
-                    </div>
-                    <div className="pr-7 py-2 space-y-1">
-                        <h3 className="font-bold">New RFQ!</h3>
-                        <p className="text-greyDark">Mark John just created a new RFQ request</p>
-                    </div>
-                    <div className="relative py-2 pr-2 pl-2 border-l-[3px] border-gray-200">
-                        <div className="text-primaryBlue w-max h-1/2 flex items-center">See Details</div>
-                        <hr className="h-[3px] w-full absolute top-[50%] left-0 bg-gray-200"/>
-                        <div className="text-greyDark h-1/2 flex justify-center items-center">Hide</div>
-                    </div>
-                </div>
-                <div className={`bg-white pl-2 flex rounded translate-x-[150%] transition ${slideIn && "translate-x-0"}`}>
-                    <div className="py-2">
-                        <NotificationSvg className="pr-2"/>
-                    </div>
-                    <div className="pr-7 py-2 space-y-1">
-                        <h3 className="font-bold">New RFQ!</h3>
-                        <p className="text-greyDark">Mark John just created a new RFQ request</p>
-                    </div>
-                    <div className="relative py-2 pr-2 pl-2 border-l-[3px] border-gray-200">
-                        <div className="text-primaryBlue w-max h-1/2 flex items-center">See Details</div>
-                        <hr className="h-[3px] w-full absolute top-[50%] left-0 bg-gray-200"/>
-                        <div className="text-greyDark h-1/2 flex justify-center items-center">Hide</div>
-                    </div>
-                </div>
-                <div className={`bg-white pl-2 flex rounded translate-x-[150%] transition ${slideIn && "translate-x-0"}`}>
-                    <div className="py-2">
-                        <NotificationSvg className="pr-2"/>
-                    </div>
-                    <div className="pr-7 py-2 space-y-1">
-                        <h3 className="font-bold">New RFQ!</h3>
-                        <p className="text-greyDark">Mark John just created a new RFQ request</p>
-                    </div>
-                    <div className="relative py-2 pr-2 pl-2 border-l-[3px] border-gray-200">
-                        <div className="text-primaryBlue w-max h-1/2 flex items-center">See Details</div>
-                        <hr className="h-[3px] w-full absolute top-[50%] left-0 bg-gray-200"/>
-                        <div className="text-greyDark h-1/2 flex justify-center items-center">Hide</div>
-                    </div>
-                </div>
-                <div className={`bg-white pl-2 flex rounded translate-x-[150%] transition ${slideIn && "translate-x-0"}`}>
-                    <div className="py-2">
-                        <NotificationSvg className="pr-2"/>
-                    </div>
-                    <div className="pr-7 py-2 space-y-1">
-                        <h3 className="font-bold">New RFQ!</h3>
-                        <p className="text-greyDark">Mark John just created a new RFQ request</p>
-                    </div>
-                    <div className="relative py-2 pr-2 pl-2 border-l-[3px] border-gray-200">
-                        <div className="text-primaryBlue w-max h-1/2 flex items-center">See Details</div>
-                        <hr className="h-[3px] w-full absolute top-[50%] left-0 bg-gray-200"/>
-                        <div className="text-greyDark h-1/2 flex justify-center items-center">Hide</div>
-                    </div>
-                </div>
-                <div className={`bg-white pl-2 flex rounded translate-x-[150%] transition ${slideIn && "translate-x-0"}`}>
-                    <div className="py-2">
-                        <NotificationSvg className="pr-2"/>
-                    </div>
-                    <div className="pr-7 py-2 space-y-1">
-                        <h3 className="font-bold">New RFQ!</h3>
-                        <p className="text-greyDark">Mark John just created a new RFQ request</p>
-                    </div>
-                    <div className="relative py-2 pr-2 pl-2 border-l-[3px] border-gray-200">
-                        <div className="text-primaryBlue w-max h-1/2 flex items-center">See Details</div>
-                        <hr className="h-[3px] w-full absolute top-[50%] left-0 bg-gray-200"/>
-                        <div className="text-greyDark h-1/2 flex justify-center items-center">Hide</div>
-                    </div>
-                </div>
-                <div className={`bg-white pl-2 flex rounded translate-x-[150%] transition ${slideIn && "translate-x-0"}`}>
-                    <div className="py-2">
-                        <NotificationSvg className="pr-2"/>
-                    </div>
-                    <div className="pr-7 py-2 space-y-1">
-                        <h3 className="font-bold">New RFQ!</h3>
-                        <p className="text-greyDark">Mark John just created a new RFQ request</p>
-                    </div>
-                    <div className="relative py-2 pr-2 pl-2 border-l-[3px] border-gray-200">
-                        <div className="text-primaryBlue w-max h-1/2 flex items-center">See Details</div>
-                        <hr className="h-[3px] w-full absolute top-[50%] left-0 bg-gray-200"/>
-                        <div className="text-greyDark h-1/2 flex justify-center items-center">Hide</div>
-                    </div>
-                </div>
-                <div className={`bg-white pl-2 flex rounded translate-x-[150%] transition ${slideIn && "translate-x-0"}`}>
-                    <div className="py-2">
-                        <NotificationSvg className="pr-2"/>
-                    </div>
-                    <div className="pr-7 py-2 space-y-1">
-                        <h3 className="font-bold">New RFQ!</h3>
-                        <p className="text-greyDark">Mark John just created a new RFQ request</p>
-                    </div>
-                    <div className="relative py-2 pr-2 pl-2 border-l-[3px] border-gray-200">
-                        <div className="text-primaryBlue w-max h-1/2 flex items-center">See Details</div>
-                        <hr className="h-[3px] w-full absolute top-[50%] left-0 bg-gray-200"/>
-                        <div className="text-greyDark h-1/2 flex justify-center items-center">Hide</div>
-                    </div>
-                </div>
-                <div className={`bg-white pl-2 flex rounded translate-x-[150%] transition ${slideIn && "translate-x-0"}`}>
-                    <div className="py-2">
-                        <NotificationSvg className="pr-2"/>
-                    </div>
-                    <div className="pr-7 py-2 space-y-1">
-                        <h3 className="font-bold">New RFQ!</h3>
-                        <p className="text-greyDark">Mark John just created a new RFQ request</p>
-                    </div>
-                    <div className="relative py-2 pr-2 pl-2 border-l-[3px] border-gray-200">
-                        <div className="text-primaryBlue w-max h-1/2 flex items-center">See Details</div>
-                        <hr className="h-[3px] w-full absolute top-[50%] left-0 bg-gray-200"/>
-                        <div className="text-greyDark h-1/2 flex justify-center items-center">Hide</div>
-                    </div>
-                </div>
+                
             </div>
         </div>
     </>
